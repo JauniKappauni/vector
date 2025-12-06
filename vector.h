d2bb9dda84c6b09c7d9630847e3f753650d03e68
JauniKappauni <social.jauni@gmail.com>
2025-12-06 16:07:36 +0100

    feat: add vector function headers

--- vector.h
+++ vector.h
@@ -1,7 +1,84 @@
 class vector
 {
 private:
+    unsigned int size;
+    unsigned int capacity;
+    int *data;
+
 public:
-    vector();
-    ~vector();
+    vector()
+    {
+        size = 0;
+        capacity = 1;
+        data = new int[capacity];
+    };
+    ~vector()
+    {
+        delete data;
+    };
+
+    void assign()
+    {
+    }
+    void at()
+    {
+    }
+    void back()
+    {
+    }
+    void begin()
+    {
+    }
+    void getCapacity()
+    {
+    }
+    void clear()
+    {
+    }
+    void getData()
+    {
+    }
+    void empty()
+    {
+    }
+    void end()
+    {
+    }
+    void erase()
+    {
+    }
+    void front()
+    {
+    }
+    void insert()
+    {
+    }
+    void max_size()
+    {
+    }
+    void pop_back()
+    {
+    }
+    void push_back()
+    {
+    }
+    void rbegin()
+    {
+    }
+    void rend()
+    {
+    }
+    void reserve()
+    {
+    }
+    void resize()
+    {
+    }
+    void shrink_to_fit()
+    {
+    }
+    void getSize()
+    {
+    }
+    void swap() {}
 };
